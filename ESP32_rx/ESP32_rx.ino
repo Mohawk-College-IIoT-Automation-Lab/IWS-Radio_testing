6251889c1bf9a957801914bd46cc4034aee0e6b9
Gabe Casciano <gabecasciano@gmail.com>
2025-02-24 03:10:43 -0500

    did stuff, v tired, double check

--- ESP32_rx/ESP32_rx.ino
+++ ESP32_rx/ESP32_rx.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
+#include "Helper.h"
 
+void setup() {
+  
+  Serial.begin(9600);
+  while(!Serial) { }; // wait for serial monitor to connect
+
+  DEBUG_PRINTLN("Started Serial & debug");
+
+  DEBUG_PRINTLN("Trying E22 setup");
+  setupE22();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly
+
+  if(e22ttl.available() > 0){
+    /*DEBUG_PRINTLN("Data available from e22");
+    if(receive_packetized_message((void*)&rx_buffer, RX_BUFFER_LENGTH)){
+      DEBUG_PRINTLN("Data rx succcess");
+      message_printer((Message*)&rx_buffer, RX_BUFFER_LENGTH);
+    }
+    else{
+      DEBUG_PRINTLN("Data rx failure");
+    }
+    */
+    
+  }
 
 }
